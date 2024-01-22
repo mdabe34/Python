4d1a57bd7f11419101faf868a2f33e9ef1812c99
mdabe34 <157070762+mdabe34@users.noreply.github.com>
2024-01-22 00:17:09 -0600

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,9 @@
+# Knock-Knock Joke Program
+print("Knock Knock.")
+print("Who's There?")
+print("Tank")
+print("Tank who?")
+print("You're welcome.")
+
+# As one line of code using escape characters
+print("Knock Knock. \nWho's there?\nTank\nTank who?\nYou're welcome.")
