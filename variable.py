a2ce61b351dfc7c3dfe178aebce72e3bde2d8af4
mdabe34 <157070762+mdabe34@users.noreply.github.com>
2024-01-22 00:38:24 -0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,19 @@
+# How many siblings do you have?
+x = 2
+print("How many siblings do you have?:", x)
+
+# When were you born?
+y = 2004
+print("When were you born?:", y)
+
+# How old are you?
+z = 19
+print("How old are you?:", z)
+
+# What is your name?
+name = "Michael"
+print("What is your name?:", name)
+
+# You have a sister
+is_active = False
+print("You have a sister:", is_active)
