38a97ea907eb577ecb48172ab778f87e8114370a
mdabe34 <157070762+mdabe34@users.noreply.github.com>
2024-01-22 22:42:13 -0600

    Add files via upload

---
+++ math1.py
@@ -0,0 +1,16 @@
+# Length
+number1 = 4
+print("\nLength: ")
+print(number1)
+
+# Width
+number2 = 8
+print("\nWidth: ")
+print(number2)
+
+# Area
+number1 = 4
+number2 = 8
+number3 = 32
+
+print("The area is: " + number1 + " " + str(number2) + ", " + str(number3))
