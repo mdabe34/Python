126a81436feb11184622b26350acd3fbb3ae3fa7
mdabe34 <157070762+mdabe34@users.noreply.github.com>
2024-02-04 17:45:22 -0600

    Add files via upload

--- comment.py
+++ comment.py
@@ -5,3 +5,5 @@ print("Miles per gallon:", mpg)
 
 # Can MPG be adjusted?
 # Circle back to add more variables
+# Could we add to calculate the price of each fill up?
+# What is the average speed of the car?
