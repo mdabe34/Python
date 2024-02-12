20a63007e5feabbdb41acc142d1d47c2bb343bfa
mdabe34 <157070762+mdabe34@users.noreply.github.com>
2024-02-12 17:48:17 -0600

    Add files via upload

--- decisions.py
+++ decisions.py
@@ -1,14 +1,14 @@
 # if statements
 
 age = "19"
-print("I am 19 years old.")
-if age== "19":
-    print("I can drive a car all by myself.")
+print("I am 19 years old")
+if age >"18":
+    print("I can obtain a drivers license.")
     print("I am allowed to vote in the United States.")
 else:
-    print("If you do own a car, what kind of car is it?")
+    print("What kind of car do you drive?")
 
-if age== "21":
-    print("I am not eligible to purchase alcohol.")
+if age > "21":
+    print("I am allowed to purchase alcohol.")
 else:
     print("I am not eligible for a senior discount.")
