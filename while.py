cf540a6f848efeb7dd59c4bee451414a7cd7458e
mdabe34 <157070762+mdabe34@users.noreply.github.com>
2024-02-12 19:12:01 -0600

    Add files via upload

---
+++ while.py
@@ -0,0 +1,9 @@
+count = 0
+while count < 100:
+    print(count)
+    print("bottles of beer on the wall")
+    print("Bottles of beer") # Increment Count
+    print("Take one down, pass it around")
+    print(count)
+    print("bottles of beer on the wall")
+    count += 1
