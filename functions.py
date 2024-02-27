c023ffd5d2c7c56e93fc244a0453c85ce5f64e5c
mdabe34 <157070762+mdabe34@users.noreply.github.com>
2024-02-27 12:05:58 -0600

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,7 @@
+def square(side):
+    result = str(side*side)
+    print("The area of this square is", result, "square units.")
+def circle(radius):
+    pi = 3.14
+    area = str(pi*radius*radius)
+    print("The area of the circle is", area, "square units.")
