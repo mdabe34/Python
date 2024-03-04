620d6d8088a89a8c62e396c61b431649c4e18d76
mdabe34 <157070762+mdabe34@users.noreply.github.com>
2024-03-04 16:36:53 -0600

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,8 @@
+programming_classes = ("Intro to Python", "Advanced Python", "Database Essentials", "Web Development Basics", "Data Structures in Python", "Web Design Fundamentals")
+
+# Print each class in the tuple from above
+for class_name in programming_classes:
+    print(class_name)
+
+# Print the number of classes to the user
+print(f"Number of classes: {len(programming_classes)}")
